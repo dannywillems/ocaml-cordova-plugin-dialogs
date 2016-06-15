e9409281a672db69c1d2271560f6d6cdd86dc6e8
Danny Willems <contact@danny-willems.be>
2016-06-15 02:39:43 +0200

    Use functions instead of objects

--- cordova_dialogs.mli
+++ cordova_dialogs.mli
@@ -4,48 +4,42 @@ type button_response =
   | Back_button [@js 0]
   [@@js.enum]
 
-class notification : Ojs.t ->
-  object
-    inherit Ojs.obj
-    (* Show an alert with a button *)
-    (* alert [message] [callback] ?[title] ?[text_button] *)
-    method alert :  string                                        ->
-                    ?cb:(unit -> unit
-                      [@js.default (fun () -> ())])               ->
-                    ?title:(string [@js.default "Alert"])         ->
-                    ?button_text:(string [@js.default "OK"])      ->
-                    unit                                          ->
-                    unit
+(* Show an alert with a button *)
+(* alert [message] [callback] ?[title] ?[text_button] *)
+val alert :
+  string                                                          ->
+  ?cb:(unit -> unit [@js.default (fun () -> ())])                 ->
+  ?title:(string [@js.default "Alert"])                           ->
+  ?button_text:(string [@js.default "OK"])                        ->
+  unit                                                            ->
+  unit
+[@@js.global "navigator.notification.alert"]
 
-    (* Show a confirm with two buttons *)
-    (* confirm [message] [callback] ?[title] ?[label_button] *)
-    method confirm :  string                                      ->
-                      ?cb:(button_response -> unit
-                        [@js.default (fun e -> ())])              ->
-                      ?title:(string [@js.default "Confirm"])     ->
-                      ?button_labels:(string array [@js.default
-                        [|"OK";"Cancel"|]])                       ->
-                      unit                                        ->
-                      unit
+(* Show a confirm with two buttons *)
+(* confirm [message] [callback] ?[title] ?[label_button] *)
+val confirm :
+  string                                                          ->
+  ?cb:(button_response -> unit [@js.default (fun e -> ())])       ->
+  ?title:(string [@js.default "Confirm"])                         ->
+  ?button_labels:(string array [@js.default [|"OK" ; "Cancel"|]]) ->
+  unit                                                            ->
+  unit
+[@@js.global "navigator.notification.confirm"]
 
-    (* Show a prompt with two buttons *)
-    (* promp [message] [callback] ?[title] ?[label_button] ?[default_text] *)
-    method prompt : string                                              ->
-                    ?cb:(button_response -> unit
-                      [@js.default (fun e -> ())])                      ->
-                    ?title:(string [@js.default "Prompt"])              ->
-                    ?button_label:(string array [@js.default [|"OK";
-                      "Cancel"|]])                                      ->
-                    ?default_text:(string [@js.default ""])             ->
-                    unit                                                ->
-                    unit
+(* Show a prompt with two buttons *)
+(* promp [message] [callback] ?[title] ?[label_button] ?[default_text] *)
+val prompt :
+  string                                                          ->
+  ?cb:(button_response -> unit [@js.default (fun e -> ())])       ->
+  ?title:(string [@js.default "Prompt"])                          ->
+  ?button_label:(string array [@js.default [|"OK" ; "Cancel"|]])  ->
+  ?default_text:(string [@js.default ""])                         ->
+  unit                                                            ->
+  unit
+[@@js.global "navigator.notification.prompt"]
 
-    (* Beep *)
-    (* beep [nb_beep] *)
-    method beep   : int -> unit
-  end
-
-(* -------------------------------------------------------------------------- *)
-val t : unit -> notification
-[@@js.get "navigator.notification"]
+(* Beep *)
+(* beep [nb_beep] *)
+val beep   : int -> unit
+[@@js.global "navigator.notification.beep"]
 (* -------------------------------------------------------------------------- *)
